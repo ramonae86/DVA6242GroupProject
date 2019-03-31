69329671a13d5bee6468c5f9e30ebfcd146abc09
Zhi, Ruoyu <rzhi6@gatech.edu>
2019-03-31 15:25:37 -0400

    combine bus and subway script

--- src/traffic_scoring.py
+++ src/traffic_scoring.py
@@ -2,6 +2,8 @@ import pandas as pd
 from math import cos, asin, sqrt
 
 effective_radius = 3
+bus_data = pd.read_csv("../data/CleanBus.csv")
+subway_data = pd.read_csv("../data/CleanSubway.csv")
 
 def get_distance(lat1, lon1, lat2, lon2):
     p = 0.017453292519943295     #Pi/180
@@ -9,31 +11,37 @@ def get_distance(lat1, lon1, lat2, lon2):
     return 12742 * asin(sqrt(a)) #2*R*asin...
 
 
-def get_bus_weight(distance):
+def get_weight(distance):
     if distance >= effective_radius:
         return 0
     else:
         return (-1/effective_radius * distance + 1)
 
 
-def get_score(coordinate):
-    bus_data = pd.read_csv("../data/CleanBus.csv")
+def get_bus_score(coordinate):
     score = 0
-
     for index, row in bus_data.iterrows():
         distance = get_distance(coordinate[0], coordinate[1], row['latitude'], row['longitude'])
-        weight = get_bus_weight(distance)
+        weight = get_weight(distance)
         score = max(score, weight)
+    return score
 
 
+def get_subway_score(coordinate):
+    score = 0
+    for index, row in subway_data.iterrows():
+        distance = get_distance(coordinate[0], coordinate[1], row['latitude'], row['longitude'])
+        weight = get_weight(distance) * row['stop_weight']/10
+        score = max(score, weight)
     return score
 
 
 def main():
-    coordinates = [[40.7489182, -73.9862724], [44.477, -73.212], [40.6948892, -74.0130932], [40.6786990, -73.9514607], [40.7653642, -73.9781252]]
+    coordinates = [(40.688453, -73.986503), (40.748760, -74.005323), (40.806264, -73.950991)]
     # coordinates = [[40.7489182, -73.9862724]]
     for coordinate in coordinates:
-        print(get_score(coordinate))
+        print(get_bus_score(coordinate))
+        print(get_subway_score(coordinate))
 
 
 if __name__ == "__main__":
